386546ff535d467f230f27167ce0a92d328b10ae
AndreTkachuk <43118839+AndreTkachuk@users.noreply.github.com>
2020-04-15 01:43:10 +0300

    adding all working files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # test_store
-Tests for different languages
+Tests for different languages in browser
--- test_items.py
+++ test_items.py
@@ -1,1 +1,15 @@
-2
+import pytest
+from selenium import webdriver
+
+
+def test_looking_for_button_add_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    btn=browser.find_element_by_css_selector(".btn-add-to-basket")
+
+    assert btn != [], "button isn't found"  
+
+
+
+
+
